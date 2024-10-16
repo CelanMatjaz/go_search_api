e7755a5241b168bb6645983a0cfdf32fd4a5a3bd
CelanMatjaz <matjazcelangithub@gmail.com>
2024-10-16 22:49:10 +0200

    add validate function

---
+++ pkg/utils/validate.go
@@ -0,0 +1,154 @@
+package utils
+
+import (
+	"fmt"
+	"reflect"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func Validate(value any) []string {
+	errors := make([]string, 0)
+	v := reflect.ValueOf(value)
+
+	for i := range v.NumField() {
+		field := v.Field(i)
+		fieldType := v.Type().Field(i)
+
+		if field.Kind() == reflect.Struct {
+			errors = append(errors, Validate(field)...)
+			continue
+		}
+
+		tag := fieldType.Tag.Get("validate")
+		if tag == "" {
+			continue
+		}
+
+		errors = append(errors, validateField(field, fieldType.Name, tag)...)
+	}
+
+	return errors
+}
+
+func validateField(val reflect.Value, fieldName string, tag string) []string {
+	errors := make([]string, 0)
+	validations := strings.Split(tag, ",")
+
+	for _, validation := range validations {
+		if strings.HasPrefix(validation, "min:") {
+			if errorString := validateMin(validation, val, fieldName); errorString != "" {
+				errors = append(errors, errorString)
+			}
+		} else if strings.HasPrefix(validation, "max:") {
+			if errorString := validateMax(validation, val, fieldName); errorString != "" {
+				errors = append(errors, errorString)
+			}
+		} else if validation == "required" {
+			if errorString := validateRequired(val, fieldName); errorString != "" {
+				errors = append(errors, errorString)
+			}
+		} else if validation == "password" {
+			if errorStrings := validatePassword(val, fieldName); len(errorStrings) > 0 {
+				errors = append(errors, errorStrings...)
+			}
+		} else if validation == "email" {
+			if errorString := validateEmail(val, fieldName); errorString != "" {
+				errors = append(errors, errorString)
+			}
+		}
+	}
+
+	return errors
+}
+
+func validateMin(validate string, val reflect.Value, fieldName string) string {
+	min, _ := strconv.Atoi(strings.TrimPrefix(validate, "min:"))
+	if len(val.String()) < min {
+		return fmt.Sprintf("Field '%s' must be at least %d characters long", fieldName, min)
+	}
+	return ""
+}
+
+func validateMax(validate string, val reflect.Value, field string) string {
+	max, _ := strconv.Atoi(strings.TrimPrefix(validate, "max:"))
+	if len(val.String()) > max {
+		return fmt.Sprintf("Field '%s' must be at most %d characters long", field, max)
+	}
+	return ""
+}
+
+func validateRequired(val reflect.Value, fieldName string) string {
+	if val.String() == "" {
+		return fmt.Sprintf("Field '%s' is required", fieldName)
+	}
+	return ""
+}
+
+func validateEmail(val reflect.Value, fieldName string) string {
+	regex := `^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`
+	if ok := regexp.MustCompile(regex).MatchString(val.String()); !ok {
+		return fmt.Sprintf("Field '%s' is not a valid email", fieldName)
+	}
+	return ""
+}
+
+func validatePassword(val reflect.Value, fieldName string) []string {
+	password := val.String()
+	errors := make([]string, 0)
+
+	number := false
+	specialCharacter := false
+	upperCase := false
+	lowerCase := false
+
+	for _, c := range []byte(password) {
+		if IsNumber(c) {
+			number = true
+		} else if IsSpecialCharacter(c) {
+			specialCharacter = true
+		} else if c >= 'a' && c <= 'z' {
+			lowerCase = true
+		} else if c >= 'A' && c <= 'Z' {
+			upperCase = true
+		}
+	}
+
+	if !number {
+		errors = append(errors, fmt.Sprintf("Field '%s' requires at least one number", fieldName))
+	}
+	if !specialCharacter {
+		errors = append(errors, fmt.Sprintf("Field '%s' requires at least one special character", fieldName))
+	}
+	if !upperCase {
+		errors = append(errors, fmt.Sprintf("Field '%s' requires at least one upper case letter", fieldName))
+	}
+	if !lowerCase {
+		errors = append(errors, fmt.Sprintf("Field '%s' requires at least one lower case letter", fieldName))
+	}
+	// if len(password) < 8 || len(password) > 32 {
+	// 	errors = append(errors, "Password length is not between 8 and 32 characters long")
+	// }
+
+	return errors
+}
+
+func IsNumber(c byte) bool {
+	if c >= '0' && c <= '9' {
+		return true
+	}
+
+	return false
+}
+
+func IsSpecialCharacter(c byte) bool {
+	if (c >= '!' && c <= '/') ||
+		(c >= ':' && c <= '@') ||
+		(c >= '[' && c <= '^') ||
+		(c >= '{' && c <= '~') {
+		return true
+	}
+
+	return false
+}
---
+++ pkg/utils/validate_test.go
@@ -0,0 +1,185 @@
+package utils
+
+import (
+	"reflect"
+	"testing"
+)
+
+type TestCaseStruct struct {
+	validateString string
+	value          reflect.Value
+	shouldFail     bool
+}
+
+func TestValidateMin(t *testing.T) {
+	t.Parallel()
+
+	testCases := []TestCaseStruct{
+		{validateString: "min:0", value: reflect.ValueOf(""), shouldFail: false},
+		{validateString: "min:1", value: reflect.ValueOf(""), shouldFail: true},
+		{validateString: "min:10", value: reflect.ValueOf("TestTest"), shouldFail: true},
+		{validateString: "min:10", value: reflect.ValueOf("TestTestTestTest"), shouldFail: false},
+		{validateString: "min:", value: reflect.ValueOf(""), shouldFail: false},
+	}
+
+	for i, tc := range testCases {
+		errorString := validateMin(tc.validateString, tc.value, "Field")
+		if (errorString != "") != tc.shouldFail {
+			t.Errorf(
+				"validateMin() failed for test case with index %d, validation: '%s', value '%s', should fail: %t",
+				i, tc.validateString, tc.value.String(), tc.shouldFail)
+		}
+	}
+}
+
+func TestValidateMax(t *testing.T) {
+	t.Parallel()
+
+	testCases := []TestCaseStruct{
+		{validateString: "max:0", value: reflect.ValueOf(""), shouldFail: false},
+		{validateString: "max:1", value: reflect.ValueOf(""), shouldFail: false},
+		{validateString: "max:0", value: reflect.ValueOf("A"), shouldFail: true},
+		{validateString: "max:1", value: reflect.ValueOf("AA"), shouldFail: true},
+		{validateString: "max:", value: reflect.ValueOf(""), shouldFail: false},
+		{validateString: "max:", value: reflect.ValueOf("A"), shouldFail: true},
+	}
+
+	for i, tc := range testCases {
+		errorString := validateMax(tc.validateString, tc.value, "Field")
+		if (errorString != "") != tc.shouldFail {
+			t.Errorf(
+				"validateMax() failed for test case with index %d, validation: '%s', value '%s', should fail: %t",
+				i, tc.validateString, tc.value.String(), tc.shouldFail)
+		}
+	}
+}
+
+func TestValidatePassword(t *testing.T) {
+	t.Parallel()
+
+	testCases := []TestCaseStruct{
+		// Check length
+		{value: reflect.ValueOf("Passwo1!"), shouldFail: false},                         // valid with 8 characters
+		{value: reflect.ValueOf("Passw1!"), shouldFail: true},                           // valid with 7 characters
+		{value: reflect.ValueOf("Passwo1!Passwo1!Passwo1!Passwo1!"), shouldFail: false}, // valid with 32 characters
+		{value: reflect.ValueOf("Passwo1!Passwo1!Passwo1!Passwo1!_"), shouldFail: true}, // valid with 33 characters
+
+		// Check for number
+		{value: reflect.ValueOf("Passwo1!"), shouldFail: false},
+		{value: reflect.ValueOf("Passwor!"), shouldFail: true},
+
+		// Check for special character
+		{value: reflect.ValueOf("Passwo1!"), shouldFail: false},
+		{value: reflect.ValueOf("Passwor1"), shouldFail: true},
+
+		// Check for lower case character
+		{value: reflect.ValueOf("Passwo1!"), shouldFail: false},
+		{value: reflect.ValueOf("PASSWO1!"), shouldFail: true},
+
+		// Check for upper case character
+		{value: reflect.ValueOf("Passwo1!"), shouldFail: false},
+		{value: reflect.ValueOf("passwo1!"), shouldFail: true},
+	}
+
+	for i, tc := range testCases {
+		errorStrings := validatePassword(tc.value, "Field")
+		if (len(errorStrings) > 0) && !tc.shouldFail {
+			t.Errorf(
+				"validatePassword() failed for test case with index %d, value '%s', should fail: %t, error message count: %d",
+				i, tc.value.String(), tc.shouldFail, len(errorStrings))
+		}
+	}
+}
+
+func TestValidateRequired(t *testing.T) {
+	t.Parallel()
+
+	testCases := []TestCaseStruct{
+		{value: reflect.ValueOf(""), shouldFail: true},
+		{value: reflect.ValueOf("Required"), shouldFail: false},
+	}
+
+	for i, tc := range testCases {
+		errorString := validateRequired(tc.value, "Field")
+		if (errorString != "") != tc.shouldFail {
+			t.Errorf(
+				"validateRequired() failed for test case with index %d, value '%s', should fail: %t",
+				i, tc.value.String(), tc.shouldFail)
+		}
+	}
+}
+
+func TestValidateEmail(t *testing.T) {
+	t.Parallel()
+
+	testCases := []TestCaseStruct{
+		{value: reflect.ValueOf("test@test.com"), shouldFail: false},
+		{value: reflect.ValueOf("test@test.co"), shouldFail: false},
+		{value: reflect.ValueOf("test@test.c"), shouldFail: true},
+		{value: reflect.ValueOf("@test.com"), shouldFail: true},
+		{value: reflect.ValueOf("test@.com"), shouldFail: true},
+		{value: reflect.ValueOf("test.com"), shouldFail: true},
+	}
+
+	for i, tc := range testCases {
+		errorString := validateEmail(tc.value, "Field")
+		if (errorString != "") != tc.shouldFail {
+			t.Errorf(
+				"validateEmail() failed for test case with index %d, value '%s', should fail: %t",
+				i, tc.value.String(), tc.shouldFail)
+		}
+	}
+}
+
+func TestValidate(t *testing.T) {
+	t.Parallel()
+
+	type EmailStruct struct {
+		email string `validate:"email"`
+	}
+
+	type RequiredStruct struct {
+		required string `validate:"required"`
+	}
+
+	type PasswordStruct struct {
+		password string `validate:"password"`
+	}
+
+	type MinMaxStruct struct {
+		field1 string `validate:"min:1,max:10"`
+		field2 string `validate:"min:3,max:6"`
+	}
+
+	type Mixed struct {
+		email    string `validate:"required,email,min:2,max:100"`
+		password string `validate:"required,min:8,max:10"`
+	}
+
+	type ValidateTestCase struct {
+		errors     []string
+		shouldFail bool
+	}
+
+	testCases := []ValidateTestCase{
+		{errors: Validate(EmailStruct{email: "test@test.com"}), shouldFail: false},
+		{errors: Validate(EmailStruct{email: ""}), shouldFail: true},
+		{errors: Validate(RequiredStruct{required: "required"}), shouldFail: false},
+		{errors: Validate(RequiredStruct{required: ""}), shouldFail: true},
+		{errors: Validate(PasswordStruct{password: ""}), shouldFail: true},
+		{errors: Validate(PasswordStruct{password: "password"}), shouldFail: true},
+		{errors: Validate(PasswordStruct{password: "Passwo1!"}), shouldFail: false},
+		{errors: Validate(MinMaxStruct{field1: "AAAA", field2: "AAAA"}), shouldFail: false},
+		{errors: Validate(MinMaxStruct{field1: "AA", field2: "AA"}), shouldFail: true},
+		{errors: Validate(Mixed{email: "AA", password: "AAAAAAAAAAAAA"}), shouldFail: true},
+		{errors: Validate(Mixed{email: "ok@email.com", password: "AAAAAAAa1!"}), shouldFail: false},
+	}
+
+	for i, tc := range testCases {
+		if (len(tc.errors) > 0) && !tc.shouldFail {
+			t.Errorf(
+				"Validate() failed for test case with index %d, should fail: %t, error count: %d",
+				i, tc.shouldFail, len(tc.errors))
+		}
+	}
+}
